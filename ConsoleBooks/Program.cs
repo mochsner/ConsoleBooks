418b5ba440a6dc4b21d5faba9f80df8549434969
Marc Ochsner <marcaochsner@gmail.com>
2019-11-03 20:57:51 -0600

    Add project files.

---
+++ ConsoleBooks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
