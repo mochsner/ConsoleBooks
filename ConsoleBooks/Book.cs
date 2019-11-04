1c44e27412af2e7d28ed567ad290716fa752ad3c
Marc Ochsner <marcaochsner@gmail.com>
2019-11-04 00:07:41 -0600

    Basic Request Handle Setup. Book.cs needing reformatting.

--- ConsoleBooks/Book.cs
+++ ConsoleBooks/Book.cs
@@ -6,5 +6,33 @@ namespace ConsoleBooks
 {
     class Book
     {
+        public String title;
+        public String[] author;
+        public String[] publisher;
+
+        public Book(string title, string author, string publisher)
+        {
+            this.title = title;
+            this.author[0] = author;
+            this.publisher[0] = publisher;
+        }
+        public Book(string title, string author, string[] publisher)
+        {
+            this.title = title;
+            this.author[0] = author;
+            this.publisher = publisher;
+        }
+        public Book(string title, string[] author, string publisher)
+        {
+            this.title = title;
+            this.author = author;
+            this.publisher[0] = publisher;
+        }
+        public Book(string title, string[] author, string[] publisher)
+        {
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+        }
     }
 }
--- ConsoleBooks/Program.cs
+++ ConsoleBooks/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Threading;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ConsoleBooks
 {
@@ -45,21 +47,75 @@ namespace ConsoleBooks
 
             } while (query.Length <= 0);
 
-            //TODO Search!
-            
+            // Search
+            WebResponse response = InvokeRequest(query);
+            var parsedResponse = HandleResponse(response);
+             
+        }
+        /// <summary>
+        /// Invokes a Web Request
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        private WebResponse InvokeRequest(String query)
+        {
+            String baseURI = "https://www.googleapis.com/books/v1/";
+            String method = "volumes?maxResults=1&q=";
+            WebRequest request = WebRequest.Create(baseURI + method + query);
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusCode);
+
+            return response;
+        }
+        private Book[] HandleResponse(WebResponse response)
+        {
+            String responseFromServer = "";
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.  
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.  
+                responseFromServer = reader.ReadToEnd();
+            }
+
+            // Display the content.  
+            JObject jsonObject = JObject.Parse(responseFromServer);
+            JArray jsonArray = (JArray)jsonObject["items"];
+
+            Book[] bookSearch = new Book[5];
+            int i = 0;
+            foreach (var obj in jsonArray)
+            {
+                dynamic author = obj["volumeInfo"]["authors"]; // Overloading
+                dynamic publisher;
+                try
+                {
+                    publisher = obj["volumeInfo"]["publisher"];
+                }
+                catch (System.FormatException e)
+                {
+                    publisher = obj["volumeInfo]"]["author"];
+                }
+                dynamic title = obj["volumeInfo"]["title"];
+
+                Book book = new Book(title, author, publisher);
+                bookSearch[i] = book;
+                ++i;
+            }
+            return bookSearch;
         }
 
-   
         private String Menu()
         {
             string[] permittedAnswers = { "s", "search", "v", "view", "q", "quit" };
             string choice = "";
             do
             {
-                Console.WriteLine("`" +
-                    "Would you like to:`" +
-                    "[s]earch:  Search the library`" +
-                    "[v]iew:    View your reading list`" +
+                Console.WriteLine("\r\n" +
+                    "Would you like to:\r\n" +
+                    "[s]earch:  Search the library\r\n" +
+                    "[v]iew:    View your reading list\r\n" +
                     "[q]uit:    Quit");
 
                 choice = Console.ReadLine();
@@ -67,15 +123,14 @@ namespace ConsoleBooks
                 if (choice == "s" || choice == "search")
                 {
                     Search();
-
                 }
                 else if (choice == "v" || choice == "view")
                 {
-
                 }
                 else if (choice == "q" || choice == "quit")
                 {
-
+                    Console.WriteLine("Quitting Application.");
+                    //Console.WriteLine("Press any buttom to interrupt cancellation");
                 }
                 else
                 {
@@ -85,6 +140,10 @@ namespace ConsoleBooks
             } while (Array.Exists(permittedAnswers, permittedAnswer => permittedAnswer == choice));
 
             return choice;
+        }
+        private static void Quit()
+        {
+
         }
         private void View()
         {
