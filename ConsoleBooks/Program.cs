3f864539547c1ab73871e1cdf1ddbc476ca089d3
Marc Ochsner <marcaochsner@gmail.com>
2019-11-05 17:54:54 -0600

    remove unused function

--- ConsoleBooks/Program.cs
+++ ConsoleBooks/Program.cs
@@ -63,12 +63,6 @@ namespace ConsoleBooks
             return parsedResponse;
         }
 
-        //public static void Each<T>(this IEnumerable<T> ie, Action<T, int> action)
-        //{
-        //    var i = 0;
-        //    foreach (var e in ie) action(e, i++);
-        //}
-
         private String InputYesOrNo(String request, String[] permittedResponse)
         {
             String choice = "";
