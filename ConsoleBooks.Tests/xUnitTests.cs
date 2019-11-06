c14cbc6c27d2434bb38efe12aa0425432ce522ef
Marc Ochsner <marcaochsner@gmail.com>
2019-11-05 23:51:03 -0600

    add file to be used for Issue #2

---
+++ ConsoleBooks.Tests/xUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ConsoleBooks
+{
+    public class xUnitTests
+    {
+        [Fact]
+        public void FirstFact()
+        {
+
+        }
+
+        [Theory]
+        [InlineData(5, 3, 2)]
+        public void FirstTheory(int expected, int addend1, int addend2)
+        {
+            Assert.Equal(expected, addend1 + addend2);
+        }
+
+        [Fact(DisplayName = "Ignored Test - Library.cs", Skip = "")]
+        public void ThisIsIgnored()
+        {
+            //TODO: Fix this test
+        }
+    }
+}
