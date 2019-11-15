a1a6b0c855b6a2a9956571695847dfc7b3754013
Marc Ochsner <marcaochsner@gmail.com>
2019-11-14 22:46:35 -0600

    #2 - failing test

--- ConsoleBooks.Tests/ReadingList_Tests.cs
+++ ConsoleBooks.Tests/ReadingList_Tests.cs
@@ -1,30 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Text;
+
 using Xunit;
 using LINQtoCSV;
 
+using ConsoleBooks;
+
 namespace ConsoleBooks
 {
-    public class LinqToCSV
+    public class ReadingList_Tests
     {
-        [Fact]
-        public void FirstFact()
-        {
+        // [Fact]
+        // public void FirstFact()
+        // {
+        // }
 
+        [Theory(DisplayName = "Constructor + Add Book Equality Check")]
+        [InlineData("The Power of Habit", new []{"Charles Duhigg"}, "Random House")]
+        public void FirstTheory(String title, String[] author, String publisher)
+        {
+            ReadingList readingList1 = new ReadingList(title,author,publisher);
+
+            ReadingList readingList2 = new ReadingList();
+            readingList2.AddBook(title,author,publisher);
+
+            Assert.Equal(readingList1,readingList2); // This should pass (but doesn't)
         }
 
-        [Theory]
-        [InlineData(5, 3, 2)]
-        public void FirstTheory(int expected, int addend1, int addend2)
-        {
-            Assert.Equal(expected, addend1 + addend2);
-        }
-
-        [Fact(DisplayName = "Ignored Test - Library.cs", Skip = "")]
-        public void ThisIsIgnored()
-        {
-            //TODO: Fix this test
-        }
+        // [Fact(DisplayName = "Ignored Test - Library.cs", Skip = "")]
+        // public void ThisIsIgnored()
+        // {
+        //     //TODO: Fix this test
+        // }
     }
 }
