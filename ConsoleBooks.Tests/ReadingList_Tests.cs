486bf54c7cc8c1289b9a08a832b00106d536c4b2
Marc Ochsner <marcaochsner@gmail.com>
2019-11-15 01:28:22 -0600

    #2 rename

--- ConsoleBooks.Tests/ReadingList_Tests.cs
+++ ConsoleBooks.Tests/ReadingList_Tests.cs
@@ -19,7 +19,7 @@ namespace ConsoleBooks
 
         [Theory(DisplayName = "Constructor + Add Book Equality Check")]
         [InlineData("The Power of Habit", new []{"Charles Duhigg"}, "Random House")]
-        public void FirstTheory(String title, String[] author, String publisher)
+        public void CheckInitializationEquality_Theory(String title, String[] author, String publisher)
         {
             ConsoleBooks.ReadingList readingList1 = new ConsoleBooks.ReadingList(title,author,publisher);
 
@@ -29,6 +29,7 @@ namespace ConsoleBooks
             Assert.Equal(readingList1.GetReadingList()[0].title,        readingList2.GetReadingList()[0].title); // This should pass (but doesn't)
             Assert.Equal(readingList1.GetReadingList()[0].author,       readingList2.GetReadingList()[0].author); // This should pass (but doesn't)
             Assert.Equal(readingList1.GetReadingList()[0].publisher,    readingList2.GetReadingList()[0].publisher); // This should pass (but doesn't)
+
         }
 
         // [Fact(DisplayName = "Ignored Test - Library.cs", Skip = "")]
