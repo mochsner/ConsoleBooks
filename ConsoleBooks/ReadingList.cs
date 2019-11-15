e4ab213a670d3700a5ee832fa2716eb7011824a4
Marc Ochsner <marcaochsner@gmail.com>
2019-11-15 01:29:21 -0600

    #3 :construction: initial DB configuration

--- ConsoleBooks/ReadingList.cs
+++ ConsoleBooks/ReadingList.cs
@@ -2,28 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 using LINQtoCSV;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace ConsoleBooks
 {
     // [Serializable()]
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> readingList { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("Data Source=ReadingList.db");
+
+    }
     public class ReadingList
     {
         public List<Book> readingList;
+        BookContext db;
 
         public ReadingList ()
         {
             readingList = new List<Book>();
+            db = new BookContext();
         }
         public ReadingList (String title, String[] author, String publisher)
         {
             Book book = new Book(title,author,publisher);
             readingList = new List<Book>();
             readingList.Add(book);
+            db.Add(book);
+            db.SaveChangesAsync();
         }
         public void AddBook (String title, String[] author, String publisher)
         {
             Book book = new Book(title,author,publisher);
             readingList.Add(book);
+            db.Add(book);
+            db.SaveChangesAsync();
         }
 
         public List<Book> GetReadingList()
